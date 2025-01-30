855f4661aba6b4f5a14575b4fd83e8a04271f1a8
desmoris <damianesmoris@gmail.com>
2025-01-29 21:16:39 -0300

    common: linklst updated for adventjs-2021/04

--- common/linklst.c
+++ common/linklst.c
@@ -9,16 +9,63 @@ void push(Linklst *l, void *ref)
         lh->first->ref = ref;
         lh->first->next = NULL;
         return;
-    }
+    } 
 
     Head* lh = *l; 
-    lh->last = lh->last->next = (Node*)malloc(sizeof(Node));
+    if (lh->last)
+        lh->last = lh->last->next = (Node*)malloc(sizeof(Node));
+    else 
+        lh->last = (Node*)malloc(sizeof(Node));
     lh->last->ref = ref;
     lh->last->next = NULL;
 }
 
+void free_lst(Linklst *l) {
+    if (!*l) return;
+    Head* lh = *l;
+    if (lh) {
+        Node *node = lh->first;
+        Node *next_node;
+        while (node) {
+            next_node = node->next;
+            if (node->ref)
+                free(node->ref);
+            free(node);
+            node = next_node;
+        }
+    }
+    free(*l);
+    *l = NULL;
+}
+
+void nonfree_pop(Linklst *l) {
+    if (!*l || !(*l)->first){
+        free_lst(l);
+        return;
+    }
+    Head* lh = *l;
+    Node* to_remove = lh->last; 
+    if (lh->first == lh->last) {
+        lh->first = lh->last = NULL;
+        free_lst(l);
+        return;
+    } else { 
+        Node* node = lh->first;
+        while (node->next != lh->last)
+            node = node->next;
+        lh->last = node;
+        lh->last->next = NULL;
+    }
+    if (to_remove->ref) 
+        free(to_remove->ref);
+    free(to_remove); 
+}
+
+
 int length(Linklst l)
 {
+    if (!l || !l->first) 
+        return 0;
     Node* node = l->first;
     int count = 0;
     while (node) {
@@ -26,4 +73,17 @@ int length(Linklst l)
         node = node->next;
     }
     return count;
+}
+
+void* last(Linklst l) {
+    return (!l || !l->first) ? NULL : l->last->ref;
+}
+
+void* prev(Linklst l, void* ref) {
+    if (!l || !l->first)
+        return NULL;
+    Node* node = l->first;
+    while (node && node->next->ref != ref)
+        node = node->next;
+    return node ? node->ref : NULL;
 }
--- common/linklst.h
+++ common/linklst.h
@@ -10,5 +10,9 @@ typedef struct {
 
 typedef Head * Linklst;
 
+void free_lst(Linklst *l);
 void push(Linklst *l, void *ref);
+void nonfree_pop(Linklst *l);
 int length(Linklst l);
+void* last(Linklst l);
+void* prev(Linklst l, void* ref);
